0eb436e73fd13aa422004f0fdbabb839a7d68988
zhou-jk <2870037012@qq.com>
2020-05-11 08:21:31 +0800

    Update the problemset

--- Clawer/Get_Bzoj.sh
+++ Clawer/Get_Bzoj.sh
@@ -3,12 +3,12 @@
 read bgn <./begin.txt
 read end <./end.txt
 pid=$bgn
-path="https://www.lydsy.com/JudgeOnline/problem.php?id="
+path="http://www.lydsy.com/JudgeOnline/show.php?id="
 
 while [ $pid -le $end ]; do
-	echo "-------------Getting problem:$pid-------------"
-	wget -m -k -e robots=off -A="problem.php?id="$pid",problem.php?id="$pid".tmp,problem.php?id="$pid".tmp.html,png,jpg,gif,webp,bmp,pdf,rar,zip,7z,txt,doc,docx,ppt,pptx" --reject="logo.png" --load-cookies="./cookies.txt" $path$pid
-	mv "./www.lydsy.com/JudgeOnline/problem.php?id="$pid".tmp" "./www.lydsy.com/JudgeOnline/"$pid".html"
+	echo "-------------Getting show:$pid-------------"
+	wget -m -k -e robots=off -A="show.php?id="$pid",show.php?id="$pid".tmp,show.php?id="$pid".tmp.html,png,jpg,gif,webp,bmp,pdf,rar,zip,7z,txt,doc,docx,ppt,pptx" --reject="logo.png" --load-cookies="./cookies.txt" $path$pid
+	mv "./www.lydsy.com/JudgeOnline/show.php?id="$pid".tmp" "./www.lydsy.com/JudgeOnline/"$pid".html"
 	echo $pid"Done."
 	let pid=pid+1
 	sleep 0.3
